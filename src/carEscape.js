6a8f189211834820c7b93f1daff942fb27cab15f
nathanvass <nathanvass@ibm.com>
2022-10-17 11:37:40 -0400

    fix: added functionality for exit

--- src/carEscape.js
+++ src/carEscape.js
@@ -1,6 +1,5 @@
 const carEscape = (startingFloor, startingPosition, levels) => {
   const output = [];
-  for (let floor = startingFloor; floor > 0; flooor--) {}
   for (let i = startingPosition; i < levels[startingFloor].length; i++) {
     output.push("right");
   }
